6a54ac296ada9ca57518f97cc1dde92b9adcc5b6
tamasbanyasz <116582308+tamasbanyasz@users.noreply.github.com>
2023-02-02 17:50:00 +0100

    Nice Literature Book class (Book Store)

---
+++ NiceLiteratureBook.java
@@ -0,0 +1,8 @@
+package MyBooks;
+
+public class NiceLiteratureBook extends Book {
+
+	public NiceLiteratureBook(String writerFirstName, String writerLastastName, String bookTitle) {
+		super(writerFirstName, writerLastastName, bookTitle);
+	}
+}
