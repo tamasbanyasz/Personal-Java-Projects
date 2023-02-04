75d64b56e567141efe8fb151ed1037ed8e83154c
tamasbanyasz <116582308+tamasbanyasz@users.noreply.github.com>
2023-02-04 02:09:35 +0100

    Delete NiceLiteratureBook.java

--- NiceLiteratureBook.java
+++
@@ -1,8 +0,0 @@
-package MyBooks;
-
-public class NiceLiteratureBook extends Book {
-
-	public NiceLiteratureBook(String writerFirstName, String writerLastastName, String bookTitle) {
-		super(writerFirstName, writerLastastName, bookTitle);
-	}
-}
