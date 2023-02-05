f6342f68147983df0f5feaca766ff5bb0f2c41d9
tamasbanyasz <116582308+tamasbanyasz@users.noreply.github.com>
2023-02-06 00:21:29 +0100

    Anagrams

--- Anagrams.java
+++ Anagrams.java
@@ -1,42 +1,58 @@
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Scanner;
 
 public class Anagrams {
 
-	public static void main(String[] args) {
-      String firstWord = "mamaii";
-      String secondWord = "mammai";
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+            
+        String firstWord = scanner.next();
+        String secondWord = scanner.next();
+        scanner.close();
+            
+        boolean ret = isAnagram(firstWord, secondWord);
+        System.out.println( (ret) ? "Anagrams" : "Not Anagrams" );
+            
+    } 
+    
+    static boolean isAnagram(String firstWord, String secondWord) {
+        String[] arrayOfFirstWord = firstWord.toLowerCase().split("");
+        String[] arrayOfSecondWord = secondWord.toLowerCase().split("");
         
-      String[] arrayOfFirstWord = firstWord.toLowerCase().split("");
-      String[] arrayOfSecondWord = secondWord.toLowerCase().split("");
+        if(arrayOfFirstWord.length != arrayOfSecondWord.length) {
+            return false;
+        }
         
-      ArrayList<String> stringArrayOfFirstWord = new ArrayList<String>();
-      for (int i = 0; i<arrayOfFirstWord.length;i++) {
-          stringArrayOfFirstWord.add(arrayOfFirstWord[i]);
-      }
+        ArrayList<String> stringArrayOfFirstWord = new ArrayList<String>();
+        for (int i = 0; i<arrayOfFirstWord.length;i++) {
+            stringArrayOfFirstWord.add(arrayOfFirstWord[i]);
+        }
         
-      ArrayList<String> stringArrayOfSecondWord = new ArrayList<String>();
-      for (int i = 0; i<arrayOfSecondWord.length;i++) {
-        	stringArrayOfSecondWord.add(arrayOfSecondWord[i]);
-      }
+        ArrayList<String> stringArrayOfSecondWord = new ArrayList<String>();
+        for (int i = 0; i<arrayOfSecondWord.length;i++) {
+            stringArrayOfSecondWord.add(arrayOfSecondWord[i]);
+        }
         
-      Collections.sort(stringArrayOfFirstWord);
-      Collections.sort(stringArrayOfSecondWord);
+        Collections.sort(stringArrayOfFirstWord);
+        Collections.sort(stringArrayOfSecondWord);
         
-      System.out.println(stringArrayOfFirstWord);
-      System.out.println(stringArrayOfSecondWord);
+        int index = 0;
+        for (String s : stringArrayOfFirstWord) {
+            if (s.equals(stringArrayOfSecondWord.get(index))) {
+                index++;
+                
+            }
+            
+        }
         
-      int index = 0;
-      for (String s : stringArrayOfFirstWord) {
-        	if (s.equals(stringArrayOfSecondWord.get(index))) {
-            	index++;
-        	}
-      }
         
-      if (index == arrayOfSecondWord.length) {
-        	System.out.println("Anagrams.");
-      } else {
-        	System.out.println("Not anagrams.");
-      }
-	 } 
-}
+        
+        if (index == arrayOfSecondWord.length && index == arrayOfFirstWord.length) {
+            return true;
+        } 
+        return false;
+
+        
+    }
+    }
