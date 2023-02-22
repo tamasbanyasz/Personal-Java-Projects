02ac3b4c1dd35f8209c0286982d2974189125959
tamasbanyasz <116582308+tamasbanyasz@users.noreply.github.com>
2023-02-22 13:58:27 +0100

    Number System Calculator

---
+++ NumberSystemCalculator.java
@@ -0,0 +1,14 @@
+
+public class NumberSystemCalculator {
+
+	public static void main(String[] args) {
+		
+		Number number = new Number();
+		
+		number.inBinarySystem(21);
+		
+		number.octalInDecimal(2034);
+		
+	}
+
+}
